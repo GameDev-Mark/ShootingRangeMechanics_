872dbdee0a1db7aef5da3395c1001c874c67d457
GameDev-Mark <gamedev.markd@gmail.com>
2021-03-18 21:02:19 +0100

    Update target

--- ShootingRangeMechanic/Assets/Scripts/BulletImpact.cs
+++ ShootingRangeMechanic/Assets/Scripts/BulletImpact.cs
@@ -2,21 +2,64 @@
 
 public class BulletImpact : MonoBehaviour
 {
-    public ParticleSystem afterEffectBullet;
+    public Light targetLightChanger;
+    //GameObject targetBackLitght;
 
-    private void OnCollisionEnter(Collision collision)
+    Vector3 targetLightChangerVector = new Vector3(0f, 1.5f, 0.5f);
+
+    void OnCollisionEnter(Collision collision)
     {
-        if(collision.GetContact(0).otherCollider)
+        if (collision.GetContact(0).otherCollider) // destroying bullet on impact of anything // 
         {
             // paint effect where the bullet came in contact with //
-            //Instantiate(afterEffectBullet, collision.GetContact(0).otherCollider.transform);
-            Destroy(gameObject, 0.1f);
+            Destroy(gameObject);
         }
-        if(collision.GetContact(0).otherCollider.gameObject.name == "Middle")
+
+        if (collision.GetContact(0).otherCollider.gameObject.name == "Middle") // MIDDLE OF TARGET // RED
+        {
+            //collision.gameObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+            //collision.gameObject.GetComponentInParent<Animator>().SetTrigger("fallBack");
+            Debug.Log("MIDDLE HIT");
+            // 10 points hit middle target //
+
+            Light lightChanger = Instantiate(targetLightChanger, collision.gameObject.transform.position - targetLightChangerVector, Quaternion.Euler(-75f, 0f, 0f));
+            lightChanger.transform.parent = collision.gameObject.transform;
+            lightChanger.color = new Color32(255, 0, 0, 255);
+            Destroy(lightChanger, 1f);
+        }
+
+        if (collision.GetContact(0).otherCollider.gameObject.name == "Inner") // Inner layer of target // GREEN
+        {
+            //collision.gameObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+            //collision.gameObject.GetComponentInParent<Animator>().SetTrigger("fallBack");
+
+            Light lightChanger = Instantiate(targetLightChanger, collision.gameObject.transform.position - targetLightChangerVector, Quaternion.Euler(-75f, 0f, 0f));
+            lightChanger.transform.parent = collision.gameObject.transform;
+            lightChanger.color = new Color32(0, 255, 0, 255);
+            Destroy(lightChanger, 1f);
+            Debug.Log("GREEN");
+        }
+        if (collision.GetContact(0).otherCollider.gameObject.name == "Outer01") // first outer layer of target // BLUE
+        {
+            //collision.gameObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+            //collision.gameObject.GetComponentInParent<Animator>().SetTrigger("fallBack");
+
+            Light lightChanger = Instantiate(targetLightChanger, collision.gameObject.transform.position - targetLightChangerVector, Quaternion.Euler(-75f, 0f, 0f));
+            lightChanger.transform.parent = collision.gameObject.transform;
+            lightChanger.color = new Color32(0, 0, 255, 255);
+            Destroy(lightChanger, 1f);
+            Debug.Log("BLUE");
+        }
+        if (collision.GetContact(0).otherCollider.gameObject.name == "TargetWhole") // Whole layer (OUTER) of target // 
         {
             collision.gameObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
-            collision.gameObject.GetComponentInParent<Animator>().SetTrigger("fallBack");
-            // 10 points hit middle target //
+            //collision.gameObject.GetComponentInParent<Animator>().SetTrigger("fallBack");
+
+            Light lightChanger = Instantiate(targetLightChanger, collision.gameObject.transform.position - targetLightChangerVector, Quaternion.Euler(-75f, 0f, 0f));
+            lightChanger.transform.parent = collision.gameObject.transform;
+            lightChanger.color = Color.yellow;
+            Destroy(lightChanger, 1f);
+            Debug.Log("YELLOW");
         }
     }
 }
